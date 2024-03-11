67f730af0f849693142b59fc0431a06f7650071d
Asmaa Elabid <asmaa9920@hotmail.com>
2024-03-11 15:35:32 +0000

    Namespaces

---
+++ Namespaces/NamespacesVsModules/calculator.ts
@@ -0,0 +1,34 @@
+namespace Calculator {
+    export class BasicCalculator {
+        constructor(private value: number = 0) {}
+
+        add(num: number): this {
+            this.value += num;
+            return this;
+        }
+
+        subtract(num: number): this {
+            this.value -= num;
+            return this;
+        }
+
+        multiply(num: number): this {
+            this.value *= num;
+            return this;
+        }
+
+        divide(num: number): this {
+            if (num === 0) {
+                throw new Error('Cannot divide by zero');
+            }
+            this.value /= num;
+            return this;
+        }
+
+        getValue(): number {
+            return this.value;
+        }
+    }
+}
+
+export { Calculator };
---
+++ Namespaces/NamespacesVsModules/useCalculator.ts
@@ -0,0 +1,5 @@
+import { Calculator } from "./calculator";
+
+const calc = new Calculator.BasicCalculator();
+const result = calc.add(5).multiply(2).divide(10).getValue();
+console.log(result); // Output: 1
